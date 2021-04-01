1a29d55d2ef2d0bd192b69de4f56e2ce0f8cd3b2
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-04-01 16:26:48 +0100

    understanding redux with node.js

---
+++ index.js
@@ -0,0 +1,83 @@
+const redux = require("redux");
+const reduxLogger = require("redux-logger");
+
+const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
+
+const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
+const buyCake = () => {
+  return {
+    type: BUY_CAKE,
+  };
+};
+
+const buyIceCream = () => {
+  return {
+    type: BUY_ICECREAM,
+  };
+};
+
+const initialCakeState = {
+  numOfCakes: 10,
+};
+const initialIceCreamState = {
+  numOfIceCreams: 20,
+};
+//when im studiing code and catch my mind straying away i'll run a callback fuction to get focused.
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case BUY_CAKE:
+//       return {
+//         ...state,
+//         numOfCakes: state.numOfCakes - 1,
+//       };
+//     case BUY_ICECREAM:
+//       return {
+//         ...state,
+//         numOfIceCreams: state.numOfIceCreams - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
+    default:
+      return state;
+  }
+};
+const cakeReducer = (state = initialCakeState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducers = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+const store = createStore(rootReducers, applyMiddleware(logger));
+console.log("initial state", store.getState());
+const unsubscribe = store.subscribe(() => {});
+
+// store.dispath(buyCake());
+// store.dispath(buyCake());
+// store.dispath(buyCake());
+// store.dispath(buyIceCream());
+// store.dispath(buyIceCream());
+
+unsubscribe();
